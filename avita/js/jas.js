fda587cb7ecb09392b7b21d27169842f1c60e7e7
Keyur <keyur602>
2020-09-29 15:53:50 +0530

    {add service}

--- avita/js/jas.js
+++ avita/js/jas.js
@@ -12,7 +12,7 @@ function showPage() {
 
 
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "300px";
 }
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
@@ -32,4 +32,28 @@ function scrollFunction() {
 	}
 }
 
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
