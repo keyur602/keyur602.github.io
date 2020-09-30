065f83c5eb929145996e4d8e9b2f78cb5ae6e031
Keyur <keyur602>
2020-09-30 09:45:34 +0530

    {set header}

--- avita/js/jas.js
+++ avita/js/jas.js
@@ -25,10 +25,32 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
 
   	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	    document.getElementById("navbar").style.backgroundColor ="#000";
+	    document.getElementById("navbar").style.backgroundColor ="#fff";
+	    document.getElementById("button-nev").style.color ="#000000";
+	    document.getElementById("logo").style.display ="none";
+	    document.getElementById("logo1").style.display ="block";
+	    
+	    document.getElementById("navbarjs1").style.color ="#000";
+	    document.getElementById("navbarjs2").style.color ="#000";
+	    document.getElementById("navbarjs3").style.color ="#000";
+	    document.getElementById("navbarjs4").style.color ="#000";
+	    document.getElementById("navbarjs5").style.color ="#000";
+	    document.getElementById("navbarjs6").style.color ="#000";
+	    document.getElementById("navbarjs7").style.color ="#000";
   	}
 	else{
 	    document.getElementById("navbar").style.backgroundColor ="rgba(0,0,0,0.7)";
+	    document.getElementById("logo1").style.display ="none";
+	    document.getElementById("logo").style.display ="block";
+	    document.getElementById("button-nev").style.color ="#ffffff";
+
+	    document.getElementById("navbarjs1").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs2").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs3").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs4").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs5").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs6").style.color ="#c5c5c5";
+	    document.getElementById("navbarjs7").style.color ="#c5c5c5";
 	}
 }
 
@@ -48,12 +70,11 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  if (n > slides.length) {slideIndex = 2}    
+  if (n < 2) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i =i+2) {
+      slides[i].style.display = "block";  
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-2].style.display = "none";  
 }
 
