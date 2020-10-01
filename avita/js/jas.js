d2f1ed5ad42c8fd03ec3a8610ee622f189ad94a0
Keyur <keyur602>
2020-10-01 11:16:43 +0530

    {add portfoliyo}

--- avita/js/jas.js
+++ avita/js/jas.js
@@ -78,3 +78,137 @@ function showSlides(n) {
   slides[slideIndex-2].style.display = "none";  
 }
 
+
+
+
+
+function slicbtn(btuvel){
+
+	var z = btuvel.value;
+
+	if (z == "all") {
+		var all = document.getElementById("all");
+  		all.classList.add("Portfolio-btn-actve");
+
+		    var DESIGN = document.getElementById("DESIGN");
+  			DESIGN.classList.remove("Portfolio-btn-actve");
+
+			  		var DEVEL = document.getElementById("DEVELOPMENT");
+			  		DEVEL.classList.remove("Portfolio-btn-actve");
+
+						var PLUG = document.getElementById("PLUGIN");
+			  			PLUG.classList.remove("Portfolio-btn-actve");
+
+			  				var PHOTOG = document.getElementById("PHOTOGRAPHY");
+			  				PHOTOG.classList.remove("Portfolio-btn-actve");
+
+	var a =	document.getElementsByClassName("aii");
+		a[0].style.display = "block";
+	}
+	else if(z == "DESIGN"){
+		var all = document.getElementById("all");
+  		all.classList.remove("Portfolio-btn-actve");
+
+		    var DESIGN = document.getElementById("DESIGN");
+  			DESIGN.classList.add("Portfolio-btn-actve");
+
+			  		var DEVEL = document.getElementById("DEVELOPMENT");
+			  		DEVEL.classList.remove("Portfolio-btn-actve");
+
+						var PLUG = document.getElementById("PLUGIN");
+			  			PLUG.classList.remove("Portfolio-btn-actve");
+
+			  				var PHOTOG = document.getElementById("PHOTOGRAPHY");
+			  				PHOTOG.classList.remove("Portfolio-btn-actve");
+
+
+		var di = document.getElementsByClassName("disi");
+		di[0].style.display = "block";
+		var de = document.getElementsByClassName("DEVEL");
+		de[0].style.display = "none";
+		var pl = document.getElementsByClassName("PLUG");
+		pl[0].style.display = "none";
+		var ph = document.getElementsByClassName("PHOTOG");
+		ph[0].style.display = "none";
+	}
+	else if(z == "DEVELOPMENT"){
+		var all = document.getElementById("all");
+  		all.classList.remove("Portfolio-btn-actve");
+
+		    var DESIGN = document.getElementById("DESIGN");
+  			DESIGN.classList.remove("Portfolio-btn-actve");
+
+			  		var DEVEL = document.getElementById("DEVELOPMENT");
+			  		DEVEL.classList.add("Portfolio-btn-actve");
+
+						var PLUG = document.getElementById("PLUGIN");
+			  			PLUG.classList.remove("Portfolio-btn-actve");
+
+			  				var PHOTOG = document.getElementById("PHOTOGRAPHY");
+			  				PHOTOG.classList.remove("Portfolio-btn-actve");
+
+
+		var di = document.getElementsByClassName("disi");
+		di[0].style.display = "none";
+		var de = document.getElementsByClassName("DEVEL");
+		de[0].style.display = "block";
+		var pl = document.getElementsByClassName("PLUG");
+		pl[0].style.display = "none";
+		var ph = document.getElementsByClassName("PHOTOG");
+		ph[0].style.display = "none";
+	}
+	else if(z == "PLUGIN"){
+		var all = document.getElementById("all");
+  		all.classList.remove("Portfolio-btn-actve");
+
+		    var DESIGN = document.getElementById("DESIGN");
+  			DESIGN.classList.remove("Portfolio-btn-actve");
+
+			  		var DEVEL = document.getElementById("DEVELOPMENT");
+			  		DEVEL.classList.remove("Portfolio-btn-actve");
+
+						var PLUG = document.getElementById("PLUGIN");
+			  			PLUG.classList.add("Portfolio-btn-actve");
+
+			  				var PHOTOG = document.getElementById("PHOTOGRAPHY");
+			  				PHOTOG.classList.remove("Portfolio-btn-actve");
+
+
+		var di = document.getElementsByClassName("disi");
+		di[0].style.display = "none";
+		var de = document.getElementsByClassName("DEVEL");
+		de[0].style.display = "none";
+		var pl = document.getElementsByClassName("PLUG");
+		pl[0].style.display = "block";
+		var ph = document.getElementsByClassName("PHOTOG");
+		ph[0].style.display = "none";
+	}
+	else if(z == "PHOTOGRAPHY"){
+		var all = document.getElementById("all");
+  		all.classList.remove("Portfolio-btn-actve");
+
+		    var DESIGN = document.getElementById("DESIGN");
+  			DESIGN.classList.remove("Portfolio-btn-actve");
+
+			  		var DEVEL = document.getElementById("DEVELOPMENT");
+			  		DEVEL.classList.remove("Portfolio-btn-actve");
+
+						var PLUG = document.getElementById("PLUGIN");
+			  			PLUG.classList.remove("Portfolio-btn-actve");
+
+			  				var PHOTOG = document.getElementById("PHOTOGRAPHY");
+			  				PHOTOG.classList.add("Portfolio-btn-actve");
+
+
+		var di = document.getElementsByClassName("disi");
+		di[0].style.display = "none";
+		var de = document.getElementsByClassName("DEVEL");
+		de[0].style.display = "none";
+		var pl = document.getElementsByClassName("PLUG");
+		pl[0].style.display = "none";
+		var ph = document.getElementsByClassName("PHOTOG");
+		ph[0].style.display = "block";
+	}
+
+}
+
