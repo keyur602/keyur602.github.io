b580ad877abf1ddf0c2aa1a5377adaf69b02802c
keyur602 <keyurdomadiya602@gmail.com>
2021-02-23 12:43:22 +0530

    Update parmition.php

--- project/admin_panel/parmition.php
+++ project/admin_panel/parmition.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 	ob_start();
 	include('dblink.php');
 	
@@ -9,28 +9,14 @@
     $par2=mysqli_fetch_array($par1);
 
     if ($par2['logtip'] != 'admin') {
-
-        $pars = "select * from hed ";
+        $pars = "select * from addtable where `status`='1'";
         $pars1=mysqli_query($con,$pars);
         $pars2=mysqli_fetch_array($pars1);
 
-        $spars = "select * from hedtab ";
-        $spars1=mysqli_query($con,$spars);
-        $spars2=mysqli_fetch_array($spars1);
-
-        if ($pars2['status'] == '1') {
-            if ($spars2['status']=='1') {
-            }
-            else
-            {
-	           header('location:index.php');
-            }   
-        }
-        // else
-        // {   
-        //        header('location:index.php');
-        // }
-        
+        if($pars2['status']!='1'){
+            // echo $pars2['status'];
+            header('location:index.php');
+        }         
         
     }
 
