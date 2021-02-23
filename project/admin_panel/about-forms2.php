4bdaeeaa9e41a57b1119ea4c57831639bd2829e4
keyur602 <keyurdomadiya602@gmail.com>
2021-02-23 12:41:19 +0530

    Update about-forms2.php

--- project/admin_panel/about-forms2.php
+++ project/admin_panel/about-forms2.php
@@ -1,7 +1,23 @@
 <?php
     ob_start();
         include('header.php');
-    include('parmition.php');
+    // include('parmition.php');
+    $name = $_SESSION['name'];
+
+    $par = "select * from admin_data where `name`='$name'";
+    $par1=mysqli_query($con,$par);
+    $par2=mysqli_fetch_array($par1);
+
+    if ($par2['logtip'] != 'admin') {
+        $pars = "select * from addtable where `name`='About2'";
+        $pars1=mysqli_query($con,$pars);
+        $pars2=mysqli_fetch_array($pars1);
+
+        if($pars2['status']!='1'){
+            // echo $pars2['status'];
+            header('location:index.php');
+        }         
+    }
 
     if (isset($_POST['submit'])) 
     {
